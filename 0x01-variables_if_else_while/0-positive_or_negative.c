5718cd21a322dbb42dc93cb185834cdd74ec2abc
Emmaxcom <emmaxcom@yahoo.com>
2022-09-09 11:18:54 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ n = rand() - RAND_MAX/2;
 /*your code goes there */
 if(n>0)
 {
-	printf("%n is positive\n", n);
+	printf("%d is positive\n", n);
 }
 else if(n==0)
 {
-	printf("%u is zero\n", n);
+	printf("%d is zero\n", n);
 }
 else if(n<0)
 {
