372bb1ca502dd69dcca795b511673efc59cbc133
Emmaxcom <emmaxcom@yahoo.com>
2022-09-15 15:29:31 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+
+void print_number(int n)
+{
+	int num = n;
+	_putchar(num + '0');
+	_putchar('\n');
+}
