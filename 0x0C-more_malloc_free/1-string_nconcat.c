6823f9d79493f08fa27e42638d9aef020189b539
Emmaxcom <emmaxcom@yahoo.com>
2022-10-06 05:01:05 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 
 /**
  * string_nconcat - Adds specific number of bytes of one string to another
