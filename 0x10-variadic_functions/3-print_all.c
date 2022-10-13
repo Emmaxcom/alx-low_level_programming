747313a1a7aa22f154d11170d44b22f06238de22
Emmaxcom <emmaxcom@yahoo.com>
2022-10-13 13:03:11 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
 /**
  * print_c - print a char
  * @c: char to print
@@ -12,7 +11,6 @@ void print_c(va_list c)
 {
 	printf("%c", va_arg(c, int));
 }
-
 /**
  * print_s - prints a string
  * @s: string to print
@@ -22,12 +20,10 @@ void print_c(va_list c)
 void print_s(va_list s)
 {
 	char *str = va_arg(s, char *);
-
 	if (str == NULL)
 		str = "(nil)";
 	printf("%s", str);
 }
-
 /**
  * print_i - prints an int
  * @i: int to print
@@ -38,7 +34,6 @@ void print_i(va_list i)
 {
 	printf("%d", va_arg(i, int));
 }
-
 /**
  * print_f - prints a float
  * @f: float to print
@@ -49,7 +44,6 @@ void print_f(va_list f)
 {
 	printf("%f", va_arg(f, double));
 }
-
 /**
  * print_all - prints anything
  * @format: list of argument types passed to the function
@@ -68,7 +62,6 @@ void print_all(const char * const format, ...)
 	};
 	va_list valist;
 	char *separator = "";
-
 	va_start(valist, format);
 	i = 0;
 	while (format && format[i])
